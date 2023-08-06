56557b2c650bb97797d746eab479eb7d623b6051
kesamet <kesamet@gmail.com>
2023-08-06 14:11:39 +0800

    Update env

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,12 @@
 import box
 import yaml
-from dotenv import load_dotenv
 
 with open("config.yaml", "r") as f:
     CFG = box.Box(yaml.safe_load(f))
 
-_ = load_dotenv()
+try:
+    from dotenv import load_dotenv
+
+    _ = load_dotenv()
+except ModuleNotFoundError:
+    pass
