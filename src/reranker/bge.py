947eaa97915f7b5f13041f941f3deb7850d26b73
kesamet <kesamet@gmail.com>
2023-12-26 15:08:35 +0800

    Fix lint

--- src/reranker/bge.py
+++ src/reranker/bge.py
@@ -21,16 +21,25 @@ class BGEReranker(BaseReranker):
         self.model.eval()
 
     def rerank(self, query: str, passages: List[Document]) -> List[Document]:
-        pairs: List[str] = [
-            [query, passage.page_content] for passage in passages
-        ]
+        pairs: List[str] = [[query, passage.page_content] for passage in passages]
 
         with torch.no_grad():
-            inputs = self.tokenizer(pairs, padding=True, truncation=True, return_tensors='pt', max_length=512)
-            scores = self.model(**inputs, return_dict=True).logits.view(-1,).float().numpy()
+            inputs = self.tokenizer(
+                pairs,
+                padding=True,
+                truncation=True,
+                return_tensors="pt",
+                max_length=512,
+            )
+            scores = (
+                self.model(**inputs, return_dict=True)
+                .logits.view(
+                    -1,
+                )
+                .float()
+                .numpy()
+            )
 
-        sorted_pairs = sorted(
-            zip(passages, scores), key=lambda x: x[1], reverse=True
-        )
+        sorted_pairs = sorted(zip(passages, scores), key=lambda x: x[1], reverse=True)
         sorted_passages = [passage for passage, _ in sorted_pairs]
         return sorted_passages
