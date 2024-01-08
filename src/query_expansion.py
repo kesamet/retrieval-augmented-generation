e577672cbdda730ad3889837bde460ae77343c8c
kesamet <kesamet@gmail.com>
2024-01-09 07:51:23 +0800

    Fix

--- src/query_expansion.py
+++ src/query_expansion.py
@@ -20,7 +20,8 @@ Output: [/INST]"""
 def build_multiple_queries_expansion_chain(llm: LLM) -> RunnableSequence:
     template = """<s>[INST] You are a helpful, respectful and honest AI Assistant. \
 Your users are asking questions about documents. \
-Suggest up to five additional related questions to help them find the information they need, for the provided question. \
+Suggest up to five additional related questions to help them find the information they need, \
+for the provided question. \
 Suggest only short questions without compound sentences. \
 Suggest a variety of questions that cover different aspects of the topic. \
 Make sure they are complete questions, and that they are related to the original question. \
