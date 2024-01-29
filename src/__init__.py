d94596cad5ea5d08d586de1c5f61843a53fab13f
kesamet <kesamet@gmail.com>
2024-01-29 08:11:01 +0800

    Improve performance

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,5 @@
 import box
-import torch
 import yaml
 
 with open("config.yaml", "r") as f:
     CFG = box.Box(yaml.safe_load(f))
-
-if torch.backends.mps.is_available() and torch.backends.mps.is_built():
-    CFG.DEVICE = torch.device("mps")
-elif torch.cuda.is_available():
-    CFG.DEVICE = torch.device("cuda")
-else:
-    CFG.DEVICE = torch.device("cpu")
