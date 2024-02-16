85761a6c57ddc493dec399c49196ae097110749a
kesamet <kesamet@gmail.com>
2024-02-16 09:15:10 +0800

    Update

--- src/retrieval_qa.py
+++ src/retrieval_qa.py
@@ -19,7 +19,7 @@ from langchain_core.vectorstores import VectorStoreRetriever
 from langchain_community.document_transformers import EmbeddingsRedundantFilter
 
 from src import CFG
-from src.prompt_templates import QA_TEMPLATE
+from src.prompt_templates import QA_TEMPLATE, CONDENSE_QUESTION_TEMPLATE
 
 
 def build_base_retriever(vectordb: VectorStore) -> VectorStoreRetriever:
@@ -108,14 +108,6 @@ def build_retrieval_chain(
     """
     retriever = build_rerank_retriever(vectordb, reranker)
 
-    condense_question_template = """Given the following conversation and a follow up question, \
-rephrase the follow up question to be a standalone question, in its original language.
-
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Standalone question:"""
-
     retrieval_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         chain_type="stuff",
@@ -123,7 +115,7 @@ Standalone question:"""
         return_source_documents=True,
         combine_docs_chain_kwargs={"prompt": PromptTemplate.from_template(QA_TEMPLATE)},
         condense_question_prompt=PromptTemplate.from_template(
-            condense_question_template
+            CONDENSE_QUESTION_TEMPLATE
         ),
     )
     return retrieval_chain
