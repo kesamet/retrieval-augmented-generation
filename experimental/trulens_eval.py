332ba7cbca7aee4ec023e4b86e1948beabc2413a
kesamet <kesamet@gmail.com>
2024-03-12 10:25:27 +0800

    Fix lint

--- experimental/trulens_eval.py
+++ experimental/trulens_eval.py
@@ -1,7 +1,13 @@
-# Setup RAG
+import numpy as np
 from langchain.prompts import PromptTemplate
-from langchain.schema import StrOutputParser
+from langchain.schema import StrOutputParser, BaseRetriever
 from langchain.schema.runnable import RunnablePassthrough
+from trulens_eval import Tru, Feedback, TruChain
+from trulens_eval.feedback import Groundedness
+from trulens_eval.feedback.provider.langchain import Langchain
+from trulens_eval.utils.serial import all_queries
+from trulens_eval.utils.json import jsonify
+from trulens_eval.schema import Select
 
 from src.embeddings import build_base_embeddings
 from src.vectordb import load_chroma
@@ -10,6 +16,7 @@ from src.retrieval_qa import build_rerank_retriever
 from src.llms import build_llm
 
 
+# Setup RAG
 embedding_function = build_base_embeddings()
 vectordb = load_chroma(embedding_function)
 reranker = build_reranker()
@@ -25,8 +32,10 @@ Answer:"""
 
 prompt = PromptTemplate.from_template(QA_TEMPLATE)
 
+
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
+
 
 rag_chain = (
     {"context": retriever | format_docs, "question": RunnablePassthrough}
@@ -36,14 +45,6 @@ rag_chain = (
 )
 
 # Evaluate with trulens-eval
-from trulens_eval import Tru, Feedback, TruChain
-from trulens_eval.feedback import Groundedness
-from trulens_eval.feedback.provider.langchain import Langchain
-from trulens_eval.utils.serial import all_queries
-from trulens_eval.utils.json import jsonify
-from trulens_eval.schema import Select
-from langchain.schema import BaseRetriever
-
 database_url = "sqlite:///data/trulens.db"
 tru = Tru(database_url=database_url, database_redact_keys=True)
 # tru.reset_database()
@@ -60,13 +61,14 @@ for lens in all_queries(app_json):
     except Exception:
         pass
 
-context = (Select.RecordCalls + retrievers[0][0]).get_relevant_documents.rets[:].page_content
-
-# metrics
-f_qa_relevance = (
-    Feedback(provider.relevance_with_cot_reasonse, name="Answer Relevance")
-    .on_input_output()
+context = (
+    (Select.RecordCalls + retrievers[0][0]).get_relevant_documents.rets[:].page_content
 )
+
+# set feedbacks
+f_qa_relevance = Feedback(
+    provider.relevance_with_cot_reasonse, name="Answer Relevance"
+).on_input_output()
 
 f_context_relevance = (
     Feedback(provider.qs_relevance_with_cot_reasons, name="Context Relevance")
@@ -100,7 +102,7 @@ for qn in qns:
 
 
 # Results
-# Dashboard
+# dashboard
 tru.run_dashboard(port=8601)
 
 # # dataframe
