a1983ec574b6051e6cc097e864e9bfe3546109b0
Kok-Meng Tan <kesamet@gmail.com>
2024-05-11 08:59:45 +0800

    Minor refactor (#35)

--- src/query_expansion.py
+++ src/query_expansion.py
@@ -4,7 +4,7 @@ from langchain.schema import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.schema.runnable.base import RunnableSequence
 
-from src.prompt_templates import MULTI_QUERIES_TEMPLATE
+from src.prompt_templates import MULTI_QUERIES_TEMPLATE, GENERATED_RESULT_TEMPLATE
 
 
 def build_llm_chain(llm: LLM, template: str) -> RunnableSequence:
@@ -13,18 +13,15 @@ def build_llm_chain(llm: LLM, template: str) -> RunnableSequence:
     return chain
 
 
-def build_generated_result_expansion_chain(llm: LLM) -> RunnableSequence:
-    template = """<s>[INST] You are a helpful assistant. \
-Provide an example answer to the given question, that might be found in a document."
-Question: {question}
-Output: [/INST]"""
-
-    chain = {"question": RunnablePassthrough()} | build_llm_chain(llm, template)
-    return chain
-
-
 def build_multiple_queries_expansion_chain(llm: LLM) -> RunnableSequence:
     chain = {"question": RunnablePassthrough()} | build_llm_chain(
         llm, MULTI_QUERIES_TEMPLATE
+    )
+    return chain
+
+
+def build_generated_result_expansion_chain(llm: LLM) -> RunnableSequence:
+    chain = {"question": RunnablePassthrough()} | build_llm_chain(
+        llm, GENERATED_RESULT_TEMPLATE
     )
     return chain
