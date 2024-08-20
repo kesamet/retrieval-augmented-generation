7fc23b5d111ed9ca77c538fc8e45651c5d7c45a8
Tan, Kok Meng <kesamet@gmail.com>
2024-08-20 08:53:02 +0800

    Fix

--- src/agents.py
+++ src/agents.py
@@ -47,11 +47,13 @@ Thought: {agent_scratchpad}"""
 # {tools}
 
 # The way you use the tools is by specifying a json blob.
-# Specifically, this json should have a `action` key (with the name of the tool to use) and a `action_input` key (with the input to the tool going here).
+# Specifically, this json should have a `action` key (with the name of the tool to use) and a `action_input` key \
+# (with the input to the tool going here).
 
 # The only values that should be in the "action" field are: {tool_names}
 
-# The $JSON_BLOB should only contain a SINGLE action, do NOT return a list of multiple actions. Here is an example of a valid $JSON_BLOB:
+# The $JSON_BLOB should only contain a SINGLE action, do NOT return a list of multiple actions. \
+# Here is an example of a valid $JSON_BLOB:
 
 # ```
 # {{
