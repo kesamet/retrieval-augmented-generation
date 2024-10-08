b0cbd7f90943e420d64e7249bbb2afab50653f8d
Tan, Kok Meng <kesamet@gmail.com>
2024-10-08 08:51:43 +0800

    Fix

--- src/agents.py
+++ src/agents.py
@@ -1,20 +1,26 @@
 from langchain_core.prompts import PromptTemplate
 from langchain.agents import AgentExecutor, create_react_agent
-from langchain.agents.output_parsers import ReActJsonSingleInputOutputParser, ReActSingleInputOutputParser
+from langchain.agents.output_parsers import (
+    ReActJsonSingleInputOutputParser,
+    # ReActSingleInputOutputParser,
+)
 
-from src.prompt_templates import REACT_JSON_TEMPLATE, REACT_TEMPLATE
+from src.prompt_templates import (
+    REACT_JSON_TEMPLATE,
+    # REACT_TEMPLATE,
+)
 
 
 def build_agent_executor(llm, tools, chat_format):
     prompt = PromptTemplate.from_template(
-        chat_format.format(system="You are a helpful assistant.", user=REACT_TEMPLATE)
+        chat_format.format(system="You are a helpful assistant.", user=REACT_JSON_TEMPLATE)
     )
     agent = create_react_agent(
         llm=llm,
         tools=tools,
         prompt=prompt,
-        # output_parser=ReActJsonSingleInputOutputParser(),
-        output_parser=ReActSingleInputOutputParser(),
+        output_parser=ReActJsonSingleInputOutputParser(),
+        # output_parser=ReActSingleInputOutputParser(),
     )
     agent_executor = AgentExecutor(
         agent=agent,
