c3761220679e761c5f76b501fec430bee7fa548c
kesamet <kesamet@gmail.com>
2025-07-17 07:50:37 +0800

    Update

--- src/chains.py
+++ src/chains.py
@@ -4,7 +4,7 @@ Chains
 
 from typing import Callable, Optional
 
-from langchain_core.language_models import LLM
+from langchain_core.language_models import BaseLanguageModel
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import (
@@ -17,7 +17,7 @@ from pydantic import BaseModel, Field
 from src.prompt_templates import prompts
 
 
-def create_llm_chain(llm: LLM, system_prompt: str, user_prompt: str) -> Runnable:
+def create_llm_chain(llm: BaseLanguageModel, system_prompt: str, user_prompt: str) -> Runnable:
     """Creates a generic llm chain."""
     prompt = PromptTemplate.from_template(
         prompts.chat_format.format(system=system_prompt, user=user_prompt)
@@ -26,7 +26,7 @@ def create_llm_chain(llm: LLM, system_prompt: str, user_prompt: str) -> Runnable
     return llm_chain
 
 
-def create_condense_question_chain(llm: LLM) -> Runnable:
+def create_condense_question_chain(llm: BaseLanguageModel) -> Runnable:
     """Creates a chain that condenses question and chat history to create a standalone question."""
     condense_question_prompt = PromptTemplate.from_template(prompts.condense_question)
     condense_question_chain = RunnableBranch(
@@ -42,7 +42,7 @@ def create_condense_question_chain(llm: LLM) -> Runnable:
     return condense_question_chain
 
 
-def create_question_answer_chain(llm: LLM) -> Runnable:
+def create_question_answer_chain(llm: BaseLanguageModel) -> Runnable:
     """Creates a question-answer chain.
     Copied from langchain.chains.combine_documents.stuff.create_stuff_documents_chain
     """
@@ -61,7 +61,7 @@ def create_question_answer_chain(llm: LLM) -> Runnable:
 
 
 def create_guardrail_chain(
-    llm: LLM, template: str, retry_func: Optional[Callable] = None
+    llm: BaseLanguageModel, template: str, retry_func: Optional[Callable] = None
 ) -> Runnable:
     class grade(BaseModel):
         """Binary score for relevance check."""
